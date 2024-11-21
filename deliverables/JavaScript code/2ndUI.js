5dc0d269c8eb02e8144195c000912b804f732210
GregT7 <gregterrelga@gmail.com>
2024-11-20 20:41:47 -0500

    redid styling for 2nd ui page and updated first ui page to match the same format

--- deliverables/JavaScript code/2ndUI.js
+++ deliverables/JavaScript code/2ndUI.js
@@ -7,134 +7,146 @@ document.addEventListener("DOMContentLoaded", () => {
             const minuteInput = instance.calendarContainer.querySelector('.flatpickr-minute'); 
         }
     });
-    errorHandling();
-    
+
+    addErrorHandlingEventListener();
 });
 
-function errorHandling() {
+function addErrorHandlingEventListener() {
     const form = document.getElementById('issueForm');
-    const errorMessage = document.getElementById('errorMessage');
     
     form.addEventListener('submit', function(event) {
         event.preventDefault();
 
         const formGroups = document.querySelectorAll(".form-group");
-        let noIssue = true;
         for (let i = 0; i < formGroups.length; ++i) {
             let hasErrorStyling = $(formGroups[i]).hasClass("errorStyling");
             let inputArray = formGroups[i].querySelectorAll("input, select, textarea");
 
             let inputType = inputArray[0].type;
-            let isValid = inputType !== "checkbox";
+            let isValid = determineIfValid(inputArray, inputType);
             
-            for (let j = 0; j < inputArray.length; ++j) {
-                
-                let inputElement = inputArray[j];
-                if (inputElement.type === "text") {
-                    if (inputElement.id === "vin") {
-                        let regex = /[A-Za-z0-9]{17}/
-                        isValid = regex.test(inputElement.value);
-                    }
-                    else if (inputElement.id === "licensePlate") {
-                        let regex = /[A-Za-z0-9]{1,7}/
-                        isValid = regex.test(inputElement.value);
-                    }
-                }
-
-                else if (inputElement.type === "select-one") {
-                    isValid = inputElement.selectedIndex !== 0;
-                }
-
-                else if (inputElement.type === "checkbox") {
-                    isValid = isValid || inputElement.checked;
-                }
-                else if (inputElement.type === "textarea") {
-                    isValid = inputElement.value.length <= 400;
-                    isValid = isValid && inputElement.value.length > 0;
-                }
-            }
-
+            let selectionStr = getSelectionString(inputType);
             if (!hasErrorStyling && !isValid) {
                 $(formGroups[i]).addClass("errorStyling");
-                
-                let selectionStr = '';
-                let errorStr = '';
-                if (inputType === "text") {
-                    selectionStr = 'input';
-                    if (inputArray[0].id === 'vin') {
-                        errorStr += "Enter the VIN";
-                    } else if (inputArray[0].id === 'licensePlate') {
-                        errorStr += "Enter a valid license plate";
-                    }
-                    
-                } else if (inputType === 'select-one') {
-                    selectionStr = 'select';
-                    errorStr += 'Select the state where the issue occured';
-                } else if (inputType === 'checkbox') {
-                    selectionStr = 'div';
-                    errorStr += 'Select at least one';
-                } else if (inputType === "textarea") {
-                    selectionStr = 'textarea';
-                    errorStr += 'Enter a description of the issue including any relevant details';
-                }
-                
-
-                if (inputType === 'checkbox' || inputType === 'textarea') {
-                    const h3Select = formGroups[i].querySelector('h3');
-                    h3Select.style.marginBottom = '5px';
-                }
-
-                const referenceElement = formGroups[i].querySelector(selectionStr);
-                let errorTextElement = document.createElement("SPAN");
-                let htmlStr = '<span class="errorMessageStyling">';
-                htmlStr += '<i class="fas fa-exclamation-circle"></i>'
-                htmlStr += `  ${errorStr}</span>`;
-                errorTextElement.innerHTML += htmlStr;
-                referenceElement.insertAdjacentElement('beforebegin', errorTextElement);
-
-                if (selectionStr !== '') {
-                    const inputField = formGroups[i].querySelector(selectionStr);
-                    inputField.style.borderColor = 'red';
-                    inputField.addEventListener("focus", () => {
-                        inputField.style.borderColor = '#ff9900';
-                    });
-                    inputField.addEventListener("blur", () => {
-                        inputField.style.borderColor = 'red';
-                    });
-                }
+                appendErrorElements(inputType, inputArray, formGroups[i], selectionStr);
+                updateErrorBorderColoring(selectionStr, formGroups[i]);
             }
 
             if (hasErrorStyling && isValid) {
-                formGroups[i].classList.remove("errorStyling");
-                
-                const spanField = formGroups[i].querySelector('span');
-                spanField.remove();
-
-                let selectionStr = '';
-                if (inputType === 'text') {
-                    selectionStr = 'input';
-                } else if (inputType === 'select-one') {
-                    selectionStr = 'select';
-                } else if (inputType === 'checkbox') {
-                    selectionStr = 'div';
-                } else if (inputType === 'textarea') {
-                    selectionStr = 'textarea';
-                }
-
-                if (inputType === 'checkbox' || inputType === 'textarea') {
-                    const h3Select = formGroups[i].querySelector('h3');
-                    h3Select.style.marginBottom = '20px';
-                }
-
-                const selectionElement = formGroups[i].querySelector(selectionStr);
-                selectionElement.style.borderColor = 'white';
-
-                selectionElement.addEventListener("blur", () => {
-                    selectionElement.style.borderColor = 'white';
-                });
-                
+                removeErrorElements(formGroups[i], selectionStr);
             }
         }
     });
 }
- 
+
+function updateErrorBorderColoring(selectionStr, formGroup) {
+    if (selectionStr !== '') {
+        const inputField = formGroup.querySelector(selectionStr);
+        inputField.style.borderColor = 'red';
+        inputField.addEventListener("focus", () => {
+            inputField.style.borderColor = '#ff9900';
+        });
+        inputField.addEventListener("blur", () => {
+            inputField.style.borderColor = 'red';
+        });
+    }
+}
+
+function appendErrorElements(inputType, inputArray, formGroup, selStr) {
+    let errorStr = getErrorString(inputType, inputArray);
+    let selectionStr = getSelectionString(inputType);
+    
+    // create error text element
+    let errorTextElement = document.createElement("SPAN");
+    errorTextElement.classList.add("errorMessageStyling");
+    errorTextElement.id = "errorSpan";
+
+    let htmlStr = `<i class="fas fa-exclamation-circle"></i> ${errorStr}`;
+    errorTextElement.innerHTML = htmlStr;
+
+    if (inputType === 'checkbox') {
+        errorTextElement.style.marginBottom = '0px';
+    }
+
+    // insert error text element
+    const referenceElement = formGroup.querySelector(selStr);
+    referenceElement.insertAdjacentElement('beforebegin', errorTextElement);
+}
+
+function removeErrorElements(formGroup, selStr) {
+    formGroup.classList.remove("errorStyling");
+                
+    const spanField = formGroup.querySelector('#errorSpan');
+    spanField.remove();
+
+
+    const selectionElement = formGroup.querySelector(selStr);
+    selectionElement.style.borderColor = 'white';
+
+    selectionElement.addEventListener("blur", () => {
+        selectionElement.style.borderColor = 'white';
+    });
+}
+
+function getErrorString(inputType, inputArray) {
+    let errorStr = '';
+    if (inputType === "text") {
+        if (inputArray[0].id === 'vin') {
+            errorStr += "Enter the VIN";
+        } else if (inputArray[0].id === 'licensePlate') {
+            errorStr += "Enter a valid license plate";
+        }
+    } else if (inputType === 'select-one') {
+        errorStr += 'Select the state where the issue occured';
+    } else if (inputType === 'checkbox') {
+        errorStr += 'Select at least one';
+    } else if (inputType === "textarea") {
+        errorStr += 'Enter a description of the issue including any relevant details';
+    }
+    return errorStr;
+}
+
+function getSelectionString(inputType) {
+    let selectionStr = '';
+    if (inputType === 'text') {
+        selectionStr = 'input';
+    } else if (inputType === 'select-one') {
+        selectionStr = 'select';
+    } else if (inputType === 'checkbox') {
+        selectionStr = 'div';
+    } else if (inputType === 'textarea') {
+        selectionStr = 'textarea';
+    }
+    return selectionStr;
+}
+
+function determineIfValid(inputArray, inputType) {
+    let isValid = inputType !== "checkbox";
+    for (let j = 0; j < inputArray.length; ++j) {
+                
+        let inputElement = inputArray[j];
+        if (inputElement.type === "text") {
+            if (inputElement.id === "vin") {
+                let regex = /[A-Za-z0-9]{17}/
+                isValid = regex.test(inputElement.value);
+            }
+            else if (inputElement.id === "licensePlate") {
+                let regex = /[A-Za-z0-9]{1,7}/
+                isValid = regex.test(inputElement.value);
+            }
+        }
+
+        else if (inputElement.type === "select-one") {
+            isValid = inputElement.selectedIndex !== 0;
+        }
+
+        else if (inputElement.type === "checkbox") {
+            isValid = isValid || inputElement.checked;
+        }
+        else if (inputElement.type === "textarea") {
+            isValid = inputElement.value.length <= 400;
+            isValid = isValid && inputElement.value.length > 0;
+        }
+    }
+    return isValid;
+}
